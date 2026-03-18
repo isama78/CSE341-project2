e1249d87803076342d4915d674eb3b7236d8cf0f
isama78 <isa21005@byui.edu>
2026-03-18 19:35:10 -0300

    Hiding login routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,9 +4,14 @@ import passport from '../config/passport.js';
 const router = Router();
 
 // Login
-router.get('/login', passport.authenticate('github', { scope: ['user:email'] }));
+router.get('/login', (req, res, next) => {
+    /* #swagger.ignore = true */
+    passport.authenticate('github', { scope: ['user:email'] })(req, res, next);
+});
+
 
 router.get('/github/callback', (req, res, next) => {
+  // #swagger.ignore = true
   passport.authenticate('github', { 
     successRedirect: '/', 
     failureRedirect: '/login' 
@@ -15,7 +20,9 @@ router.get('/github/callback', (req, res, next) => {
 });
 
 // Logout
+
 router.get('/logout', (req, res, next) => {
+  // #swagger.ignore = true
   req.logout((err) => {
     if (err) return next(err);
     res.redirect('/');
